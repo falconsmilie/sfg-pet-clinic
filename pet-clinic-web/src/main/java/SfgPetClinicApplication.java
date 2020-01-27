3407ed97bb31fb41983c82e352f77da073dad2f6
falconsmilie <heizenburgblue@gmail.com>
2020-01-27 21:43:06 +0100

    Refactor to multi module. Closes #1

--- pet-clinic-web/src/main/java/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/SfgPetClinicApplication.java
@@ -1,5 +1,3 @@
-package guru.springframework.sfgpetclinic;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +7,4 @@ public class SfgPetClinicApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SfgPetClinicApplication.class, args);
 	}
-
 }

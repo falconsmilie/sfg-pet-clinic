0a31cdbcad03e32c1fac2edab70e844d52fa685c
falconsmilie <heizenburgblue@gmail.com>
2020-02-07 08:01:04 +0100

    Update Profiles to Use Spring Data JPA Services. Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -43,7 +43,7 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
 
     private Long getNextId() {
 
-        Long nextId = null;
+        Long nextId;
 
         try {
             nextId = Collections.max(map.keySet()) + 1;
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -29,7 +29,7 @@ public class DataLoader implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
 
         int count = petTypeService.findAll().size();
 
